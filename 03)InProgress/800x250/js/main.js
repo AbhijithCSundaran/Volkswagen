9ac6cd934041cfbf210a46e1e4f6e99ce6be008a
AbhijithCSundaran <abhijith.sundaran@annalect.com>
2024-04-23 20:30:42 +0530

    800x250

--- 03)InProgress/800x250/js/main.js
+++ 03)InProgress/800x250/js/main.js
@@ -1,11 +1,3 @@
-var cta = document.getElementsByClassName("cta");
-var stoerer = document.getElementsByClassName("stoerer");
-
-var bg = document.getElementsByClassName("bg");
-var logo = document.getElementsByClassName("logo");
-// var line = document.getElementsByClassName("line");
-
-
 // function getStyle(el, styleProp) {
 //   if (window.getComputedStyle) {
 //     var y = document.defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
@@ -27,8 +19,8 @@ var globalTimeline = null;
 
 var disableHideDat = false;
 
-var w = 800;
-var h = 250;
+// var w = 800;
+// var h = 250;
 
 var initAd = function () {
   trace("initAd");
@@ -40,10 +32,10 @@ var initAd = function () {
   gtl.set('.tf', { opacity: 0 }, cl + "+=0");
 
   gtl.set(cta, { opacity: 0, scale: 1, transformOrigin: "121px 187px" }, cl + "+=0.00");
-  gtl.set(stoerer, { x: 135, opacity: 0 }, cl + "+=0.00");
+  gtl.set('.stoerer', { x: 135, opacity: 0 }, cl + "+=0.00");
 
   gtl.addLabel("fadein", cl + "+=0.10"); cl = "fadein";
-  gtl.to(logo, 0.0, { opacity: 1, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to('.logo', 0.0, { opacity: 1, ease: Power1.easeOut }, cl + "+=0.0");
   gtl.to('.tf-disc', 0.3, { opacity: 1, ease: Power1.easeOut }, cl + "+=0.2");
 
   gtl.addLabel("phase1", cl + "+=0.5"); cl = "phase1";
@@ -54,19 +46,26 @@ var initAd = function () {
   gtl.staggerTo('.tf1', 0.3, { opacity: 1, ease: Power1.easeOut }, 1.3, cl + "+=0.8");
 
   gtl.addLabel("phase3", cl + "+=4.5"); cl = "phase3";
-  gtl.to(stoerer, 0.5, { opacity: 1, x: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to('.stoerer', 0.5, { opacity: 1, x: 0, ease: Power1.easeOut }, cl + "+=0.0");
 
   gtl.addLabel("phase4", cl + "+=4.5"); cl = "phase4";
-  gtl.to(bg[1], 1.0, { opacity: 1.0, ease: Sine.easeOut }, cl + "+=0.00");
-  gtl.to('.tf-disc', 1.0, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
-  gtl.staggerTo('.tf2', 0.3, { opacity: 1, ease: Power1.easeOut }, 0.10, cl + "+=0.8");
-  gtl.to(cta, 0.3, { opacity: 1.05, ease: Power1.easeInOut }, cl + "+=1.4");
-  gtl.to(cta, 0.2, { scale: 1.05, repeat: 3, yoyo: true, ease: Power1.easeInOut }, cl + "+=2.4");
+  gtl.to(dealer_wrap_start, 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to(btn_info, 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to(bgImg, 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to('.tf', 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to('.logo', 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to(overlay_info, 1.0, { opacity: 1.0, ease: Sine.easeOut }, cl + "+=0.00");
+  gtl.to(html_legal_text, 0.2, { opacity: 1.0, ease: Power1.easeInOut }, cl + "+=1.00");
 
-  gtl.addLabel("fadeout", cl + "+=6"); cl = "fadeout";
-  gtl.to(logo, 0.2, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.2");
+  gtl.addLabel("phase5", cl + "+=5.7"); cl = "phase5";
+  gtl.to(overlay_info, 0.3, { opacity: 0.0, ease: Sine.easeOut }, cl + "+=0.00");
+  gtl.to(dealer_wrap, 0.3, { opacity: 1.0, ease: Sine.easeOut }, cl + "+=0.00");
+  gtl.staggerTo('.tf2', 0.3, { opacity: 1, ease: Power1.easeOut }, 0.10, cl + "+=1.0");
+  gtl.to(cta, 0.3, { opacity: 1.0, ease: Power1.easeInOut }, cl + "+=1.4");
+  gtl.to(cta, 0.2, { scale: 1.03, repeat: 3, yoyo: true, ease: Power1.easeInOut }, cl + "+=2.4");
+
+  gtl.addLabel("fadeout", cl + "+=6.5"); cl = "fadeout";
   gtl.to(cta, 0.2, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.1");
-  gtl.to(bg[1], 0.5, { scale: 1.6, ease: Power1.easeOut }, cl + "+=0.0");
 
   gtl.seek("fadein");
 
@@ -142,7 +141,7 @@ function playFullLoop() {
 function playLastLoop() {
   trace("playLastLoop ::: timestamp: " + (new Date()).getTime());
   showAd();
-  globalTimeline.tweenFromTo("fadein", "fadeout");
+  // globalTimeline.tweenFromTo("fadein", "fadeout");
 }
 
 
@@ -173,6 +172,7 @@ function onWindowLoaded() {
   //trace("onWindowLoaded");
   //container_ad.addEventListener("click", onClickExit);
   //handleSVData();
+
   addListener();
   initAd();
   sctData();
