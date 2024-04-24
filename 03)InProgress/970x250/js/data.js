6b4146376712bc259e51f8ef038343d2e1fd3f07
AbhijithCSundaran <abhijith.sundaran@annalect.com>
2024-04-24 17:09:25 +0530

    970x250

--- 03)InProgress/970x250/js/data.js
+++ 03)InProgress/970x250/js/data.js
@@ -1,6 +1,6 @@
 window.sctContent = {
   html_vw: "Ihr Volkswagen Partner",
-  html_dealer_headline_0_start: "Autohaus Carl Bondbauer GmbH & Co. KG langer Name",
+  html_dealer_headline_0_start: "Autohaus Carl Bondbauer GmbH & Co. KG langer Firmenzusatz",
   html_dealer_headline_0: "Autohaus Carl Bondbauer GmbH & Co. KG",
   html_dealer_address_line_0: "Musterstraße 5",
   html_dealer_address_line_1: "12345 Musterstadt",
