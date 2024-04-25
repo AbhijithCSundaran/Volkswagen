f28f66ecd4cc74ffaf43e051df41d2a17b487fa9
AbhijithCSundaran <abhijith.sundaran@annalect.com>
2024-04-25 15:32:30 +0530

    300x600

--- 03)InProgress/300x600/js/data.js
+++ 03)InProgress/300x600/js/data.js
@@ -4,7 +4,7 @@ window.sctContent = {
   html_dealer_headline_0: "Autohaus Carl Bondbauer GmbH & Co. KG",
   html_dealer_address_line_0: "Musterstraße 5",
   html_dealer_address_line_1: "12345 Musterstadt",
-  html_legal_text: "<sup>1</sup> Fahrzeugabbildung zeigt Sonderausstattungen. Bildliche Darstellungen können vom Auslieferungsstand abweichen. Änderungen und Irrtümer vorbehalten. Stand 04/2024.",
+  html_legal_text: "<sup>1</sup> Fahrzeugabbildung zeigt Sonderausstattungen. Bild-liche Darstellungen können vom Auslieferungsstand abweichen. Änderungen und Irrtümer vorbehalten. Stand 04/2024.",
 
   _landingpage: 'https://www.socoto.com'
 };
