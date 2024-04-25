ab1563ca8841c5718ef525eaa1e1d5ead4bf051d
AbhijithCSundaran <abhijith.sundaran@annalect.com>
2024-04-25 20:45:48 +0530

    main index

---
+++ 03)InProgress/GV5594/800x250/js/main.js
@@ -0,0 +1,186 @@
+// function getStyle(el, styleProp) {
+//   if (window.getComputedStyle) {
+//     var y = document.defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+//   }
+//   else if (el.currentStyle) {
+//     var y = el.currentStyle[styleProp];
+//   }
+//   return y;
+// }
+
+// var lineLeftWidth = parseInt(getStyle(line[0], "width"), 10);
+// var lineRightWidth = parseInt(getStyle(line[1], "width"), 10);
+
+
+TweenMax.set("img", { rotation: 0.05, force3D: true });
+TweenMax.set(".tl", { transformOrigin: "0% 0%" });
+
+var globalTimeline = null;
+
+var disableHideDat = false;
+
+var maxLoops = 100;
+var numLoops = 0;
+
+var initAd = function () {
+  trace("initAd");
+
+  var gtl = new TimelineMax({ paused: true, onComplete: onLoop });
+  var cl = "";
+
+  gtl.addLabel("start", cl + "+=0.00"); cl = "start";
+  gtl.set('.tf', { opacity: 0 }, cl + "+=0");
+
+  gtl.set(cta, { opacity: 0, scale: 1, transformOrigin: "121px 187px" }, cl + "+=0.00");
+  gtl.set('.stoerer', { x: 135, opacity: 0 }, cl + "+=0.00");
+
+  gtl.addLabel("fadein", cl + "+=0.10"); cl = "fadein";
+  gtl.to('.logo', 0.0, { opacity: 1, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to('.tf-disc', 0.3, { opacity: 1, ease: Power1.easeOut }, cl + "+=0.2");
+
+  gtl.addLabel("phase1", cl + "+=0.5"); cl = "phase1";
+  gtl.staggerTo('.tf0', 0.3, { opacity: 1, ease: Power1.easeOut }, 0.8, cl + "+=0.0");
+
+  gtl.addLabel("phase2", cl + "+=2.5"); cl = "phase2";
+  gtl.staggerTo('.tf0', 0.3, { opacity: 0, ease: Power1.easeOut }, 0.0, cl + "+=0.0");
+  gtl.staggerTo('.tf1', 0.3, { opacity: 1, ease: Power1.easeOut }, 1.3, cl + "+=0.8");
+
+  gtl.addLabel("phase3", cl + "+=4.5"); cl = "phase3";
+  gtl.to('.stoerer', 0.5, { opacity: 1, x: 0, ease: Power1.easeOut }, cl + "+=0.0");
+
+  gtl.addLabel("phase4", cl + "+=4.5"); cl = "phase4";
+  gtl.to(dealer_wrap_start, 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to(btn_info, 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to(bgImg, 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to('.tf', 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to('.logo', 0.3, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.0");
+  gtl.to(overlay_info, 1.0, { opacity: 1.0, ease: Sine.easeOut }, cl + "+=0.00");
+  gtl.to(html_legal_text, 0.2, { opacity: 1.0, ease: Power1.easeInOut }, cl + "+=1.00");
+
+  gtl.addLabel("phase5", cl + "+=5.7"); cl = "phase5";
+  gtl.to(overlay_info, 0.3, { opacity: 0.0, ease: Sine.easeOut }, cl + "+=0.00");
+  gtl.to(dealer_wrap, 0.3, { opacity: 1.0, ease: Sine.easeOut }, cl + "+=0.00");
+  gtl.staggerTo('.tf2', 0.3, { opacity: 1, ease: Power1.easeOut }, 0.10, cl + "+=1.0");
+  gtl.to(cta, 0.3, { opacity: 1.0, ease: Power1.easeInOut }, cl + "+=1.4");
+  gtl.to(cta, 0.2, { scale: 1.03, repeat: 3, yoyo: true, ease: Power1.easeInOut }, cl + "+=2.4");
+
+  gtl.addLabel("fadeout", cl + "+=6.5"); cl = "fadeout";
+  gtl.to(cta, 0.2, { opacity: 0, ease: Power1.easeOut }, cl + "+=0.1");
+
+  gtl.seek("fadein");
+
+  trace("animation length (one loop) ::: " + gtl.duration() + " ::: maxLoops ::: " + maxLoops);
+
+  globalTimeline = gtl;
+  gtl.timeScale(1);
+
+};
+
+function showDAT(e) {
+  if (e) {
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+  }
+  console.log("clicked");
+  TweenMax.to(overlay_info, 0.3, { opacity: 1, ease: Power2.easeOut });
+  TweenMax.to(content, 0.3, { opacity: 0, ease: Power2.easeIn });
+  TweenMax.to(btn_info, 0.3, { opacity: 0, ease: Power2.easeIn });
+  TweenMax.to(btn_close, 0.3, { opacity: 1, ease: Power2.easeIn });
+  close_pane.style.display = "block";
+
+}
+
+function hideDAT(e) {
+  if (e) {
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+  }
+
+  if (!disableHideDat) {
+    TweenMax.to(overlay_info, 0.3, { opacity: 0, ease: Power2.easeIn });
+    TweenMax.to(btn_close, 0.3, { opacity: 0, ease: Power2.easeIn });
+    close_pane.style.display = "none";
+    TweenMax.to(content, 0.3, { opacity: 1, ease: Power2.easeOut });
+    TweenMax.to(btn_info, 0.3, { opacity: 1, ease: Power2.easeOut });
+  }
+
+}
+
+function addListener() {
+  dat_pane.addEventListener("click", showDAT);
+  close_pane.addEventListener("click", hideDAT);
+}
+
+var onLoop = function () {
+
+  if ((numLoops == 1 && maxLoops == 1) || (maxLoops > 0 && numLoops > maxLoops)) {
+    return;
+  }
+
+  numLoops++;
+
+  trace("onLoop ::: loop " + numLoops + " of " + maxLoops);
+
+  if (maxLoops == -1 || numLoops < maxLoops) {
+    playFullLoop();
+  } else if (numLoops == maxLoops) {
+    playLastLoop();
+  }
+};
+
+function playFullLoop() {
+  trace("playFullLoop ::: timestamp: " + (new Date()).getTime());
+  showAd();
+  globalTimeline.play("fadein");
+  // globalTimeline.seek("fadeout");
+}
+
+function playLastLoop() {
+  trace("playLastLoop ::: timestamp: " + (new Date()).getTime());
+  showAd();
+  // globalTimeline.tweenFromTo("fadein", "fadeout");
+}
+
+
+function showAd() {
+  TweenMax.set(container_ad, { display: "block" });
+  TweenMax.to(container_ad, 0.3, { opacity: 1 });
+}
+
+
+
+
+
+function onClickExit() {
+  trace("exitClickHandler");
+  EB.clickthrough();
+}
+
+function handleSVData(data) {
+  document.getElementById("html_dealer_headline_0_start").innerHTML = adkit.getSVData("dc_dealer_headline_0");
+  document.getElementById("html_dealer_headline_0").innerHTML = adkit.getSVData("dc_dealer_headline_0");
+  document.getElementById("html_dealer_address_line_0").innerHTML = adkit.getSVData("dc_dealer_address_line_0");
+  document.getElementById("html_dealer_address_line_1").innerHTML = adkit.getSVData("dc_dealer_address_line_1");
+
+}
+
+
+function onWindowLoaded() {
+  //trace("onWindowLoaded");
+  //container_ad.addEventListener("click", onClickExit);
+  //handleSVData();
+
+  addListener();
+  initAd();
+  sctData();
+  onLoop();
+}
+
+window.onload = onWindowLoaded;
+
+
+//   function initAdKit() {
+//       adkit.onReady(onWindowLoaded);
+//   }
+//
+// window.onload = initAdKit;
---
+++ 03)InProgress/GV5594/800x250/js/sctVars.js
@@ -0,0 +1,71 @@
+
+// Die	Funktion sctData()	sollte	nach	der	Initialisierung	des	HTML5-Banners	aufgerufen	werden.	Diese
+// Funktion	�bertr�gt	die	Werte	aus	dem	Content	Object	auf	die	Tags	im	HTML5-Banner.	Im	Google
+// Web	Designer	steht	dazu zum	Beispiel	die	Funktion	handleDomContentLoaded zur	Verf�gung:
+
+
+function sctData(params) {
+    //	Wenn	Werte	nicht	als	Funktionsparameter	uebergeben	werden,
+    //	GET-Parameter	benutzen
+    if (!params) {
+        params = window.location.search.substring(1);
+    }
+    //	Wenn	'params'	vorhanden,	die	Werte	in	Content-Object	einlesen
+    //	Key	=	ID	von	DIV	Tag,	Value	=	Inhalt
+    if (params) {
+        window.sctContent = {};
+        var s = params.split('&');
+        if (!s.length) return;
+        for (var i = 0; i < s.length; i++) {
+            var parts = s[i].split('=');
+            window.sctContent[unescape(parts[0])] =
+                unescape(decodeURIComponent(parts[1]));
+        }
+    }
+
+
+    //	Ansonsten	wird	angenommen,	Content-Object	wurde	ueber
+    //	Einbindung	einer	JS	Datei	gesetzt
+    if (window.sctContent) {
+        for (var key in window.sctContent) {
+            if (window.sctContent.hasOwnProperty(key)) {
+                sctSetValue(key, window.sctContent[key]);
+            }
+        }
+    }
+    fillRepetiveContent();
+}
+
+function fillRepetiveContent(){
+    document.getElementById("html_vw_start").innerHTML = html_vw.innerHTML;
+    // document.getElementById("html_dealer_headline_0_start").innerHTML = html_dealer_headline_0.innerHTML;
+}
+
+/**
+ *    Setzt    einen    Wert
+ */
+function sctSetValue(key, value) {
+    var elem = document.getElementById(key);
+    if (elem != null) {
+            elem.innerHTML = value;
+        }
+    }
+
+
+/**
+ *   Google Web Designer
+ *    Laesst    das    Banner    zu    einer    bestimmten    Szene    Springen    und    prueft    anschliessend
+ *    ob    die    Szene    auch    tatsaechlich    gesetzt    wurde
+ */
+function sctGotoLabel (szene) {
+switch (szene) {
+	case "Adresse":
+      globalTimeline.tweenFromTo("phase4", "fadeout");
+			return true;
+			break;
+			}
+			return false;
+	}
+
+
+
