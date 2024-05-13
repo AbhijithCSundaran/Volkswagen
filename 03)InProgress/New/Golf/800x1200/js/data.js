52f39c63090e9ffd97465891a6a18f861cbbc354
AbhijithCSundaran <abhijith.sundaran@annalect.com>
2024-05-13 17:36:18 +0530

    banner golf 800x1200

---
+++ 03)InProgress/New/Golf/800x1200/js/data.js
@@ -0,0 +1,19 @@
+window.sctContent = {
+  html_vw: "Ihr Volkswagen Partner",
+  html_dealer_headline_0_start: "Autohaus Carl Bondbauer GmbH & Co. KG langer Firmenzusatz",
+  html_dealer_headline_0: "Carl Bondbauer GmbH & Co. KG",
+  html_dealer_address_line_0: "Musterstraße 5",
+  html_dealer_address_line_1: "12345 Musterstadt",
+  html_legal_text: "<sup>1</sup> Angaben zu Verbrauch, CO₂-Emissionen und CO₂-Klassen bei Spannbreiten in Abhängigkeit von den gewählten Ausstattungen des Fahrzeugs. Fahrzeugabbildung zeigt Sonderausstattungen. Änderungen und Irrtümer vorbehalten. Stand 04/2024.",
+
+  _landingpage: 'https://www.socoto.com'
+};
+
+
+
+
+
+
+
+
+
---
+++ 03)InProgress/New/Tiguan/300x250/js/data.js
@@ -0,0 +1,19 @@
+window.sctContent = {
+  html_vw: "Ihr Volkswagen Partner",
+  html_dealer_headline_0_start: "Autohaus Carl Bondbauer GmbH & Co. KG langer",
+  html_dealer_headline_0: "Autohaus Carl Bondbauer GmbH & Co. KG",
+  html_dealer_address_line_0: "Musterstraße 5",
+  html_dealer_address_line_1: "12345 Musterstadt",
+  html_legal_text: "<sup>1</sup> Fahrzeugabbildung zeigt Sonderausstattungen. Bild-liche Darstellungen können vom Auslieferungsstand abweichen. Änderungen und Irrtümer vorbehalten. Stand 04/2024.",
+
+  _landingpage: 'https://www.socoto.com'
+};
+
+
+
+
+
+
+
+
+
